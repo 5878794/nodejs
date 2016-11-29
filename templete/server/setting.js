1f4b11fca4d14dfefb5f027d995b26895d9eafbd
beens <5878794@qq.com>
2016-11-29 09:24:03 +0800

    add  mysql

--- templete/server/setting.js
+++ templete/server/setting.js
@@ -6,5 +6,12 @@
 module.exports = {
     wwwDir:"/Users/beens/code/github/nodejs/templete/www/",
     serverPort:"8001",
-    url404:"/Users/beens/code/github/nodejs/templete/www/404.html"
+    url404:"/Users/beens/code/github/nodejs/templete/www/404.html",
+    mysql:{
+        host:"localhost",
+        user: 'bens',
+        password: 'xf771026aa',
+        database:'chenhong',
+        port: 3306
+    }
 };
